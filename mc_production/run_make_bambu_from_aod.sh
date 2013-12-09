d7d429789af03c43818a9ab555b48fb3f5fbe308
Andrew <anlevin@cern.ch>
2013-12-09 18:53:10 +0100

    no commit message

--- mc_production/run_make_bambu_from_aod.sh
+++ mc_production/run_make_bambu_from_aod.sh
@@ -1,4 +1,29 @@
-for file in `/afs/cern.ch/project/eos/installation/0.2.31/bin/eos.select ls /eos/cms/store/user/anlevin/data/AOD/unmerged/ | grep 0tev-4`
+input_dir=/store/user/anlevin/data/AOD/ww_to_ll_same_sign_sm_qcd_99_qed_4/
+output_dir=/eos/cms/store/user/anlevin/data/BAMBU/ww_to_ll_same_sign_sm_qcd_99_qed_4/
+
+echo \$input_dir
+echo $input_dir
+
+echo \$output_dir
+echo $output_dir
+
+if ! /afs/cern.ch/project/eos/installation/0.2.31/bin/eos.select ls $output_dir >& /dev/null
+    then
+    echo "output directory does not exist, exiting"
+    exit
+fi
+
+outputdirsize=`/afs/cern.ch/project/eos/installation/0.2.31/bin/eos.select ls $output_dir | wc -l`
+
+if((outputdirsize!=0))
+    then
+    echo "output directory is not empty, exiting"
+    exit
+fi
+
+sleep 15
+
+for file in `/afs/cern.ch/project/eos/installation/0.2.31/bin/eos.select ls $input_dir`
   do
-  bsub -q 8nh "bash make_bambu_from_aod.sh /store/user/anlevin/data/AOD/unmerged/$file /eos/cms/store/user/anlevin/data/BAMBU/"
+  bsub -q 1nd "bash make_bambu_from_aod.sh $input_dir/$file $output_dir"
 done
