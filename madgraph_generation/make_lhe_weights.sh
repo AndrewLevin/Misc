ab151a6c059a27a6b54a6887af358e5209b0bcc6
Andrew Michael Levin <anlevin@t3btch110.mit.edu>
2014-01-31 13:10:17 -0500

    no commit message

--- madgraph_generation/make_lhe_weights.sh
+++ madgraph_generation/make_lhe_weights.sh
@@ -1,4 +1,10 @@
-seed=$1
+gen_command1=$1
+gen_command2=$2
+seed=$3
+output_dir=$4
+reweight_file=$5
+model=$6
+
 
 echo "pwd"
 pwd
@@ -8,12 +14,30 @@ echo ""
 echo \$seed
 echo $seed
 
+echo \$output_dir
+echo $output_dir
+
+echo \$reweight_file
+echo $reweight_file
+
+echo \$gen_command1
+echo $gen_command1
+
+echo \$gen_command2
+echo $gen_command2
+
+echo \$model
+echo $model
+
+
+n_events_per_job=1000
+
 if hostname | grep 'lxplus.*\.cern\.ch' >& /dev/null; then
     echo "running on lxplus"
     cp -r /afs/cern.ch/work/a/anlevin/madgraph_beta/2.0.0beta4/ .
 elif hostname | grep 'mit\.edu' &> /dev/null; then
     echo "running at MIT"
-    cp -r /scratch/anlevin/madgraph_beta/2.0.0beta4/ .
+    cp -r /scratch/anlevin/MG5_aMC_v2_0_0/ .
 else
     echo "running on unknown machine, exiting"
     exit
@@ -24,12 +48,12 @@ ls
 
 echo ""
 
-echo "ls 2.0.0beta4"
-ls 2.0.0beta4
+echo "ls MG5_aMC_v2_0_0"
+ls MG5_aMC_v2_0_0
 
 echo "" 
 
-cd 2.0.0beta4
+cd MG5_aMC_v2_0_0
 
 echo "pwd"
 pwd
@@ -39,39 +63,41 @@ echo ""
 if hostname | grep 'lxplus.*\.cern\.ch' >& /dev/null; then
     echo "running on lxplus"
     cat > commands.mg5 <<EOF
-    import model SM_LS0_LS1_UFO
-    define l+ = e+ mu+ ta+
-    define l- = e- mu- ta-
-    generate p p > w+ w+ p p QED=4 QCD=99, w+ > l+ vl
-    add process p p > w- w- p p QED=4 QCD=99, w- > l- vl~
-    output qed_4_qcd_99_ls0ls1_grid
-    launch qed_4_qcd_99_ls0ls1_grid
-    reweight=ON
-    /afs/cern.ch/work/a/anlevin/UserCode/madgraph_generation/run_card.dat
-    /afs/cern.ch/work/a/anlevin/UserCode/madgraph_generation/param_card.dat
-    /afs/cern.ch/work/a/anlevin/UserCode/madgraph_generation/reweight_card.dat
-    set clusinfo F
-    set nevents 100
-    set iseed $seed
-    EOF
+set nb_core 2
+import $model
+define l+ = e+ mu+ ta+
+define l- = e- mu- ta-
+$gen_command1
+$gen_command2
+output mg_dir
+launch mg_dir
+reweight=ON 
+/scratch/anlevin/UserCode/madgraph_generation/run_card.dat
+/scratch/anlevin/UserCode/madgraph_generation/param_card.dat
+$reweight_file
+set clusinfo F
+set nevents $n_events_per_job
+set iseed $seed
+EOF
 elif hostname | grep 'mit\.edu' &> /dev/null; then
     echo "running at MIT"
     cat > commands.mg5 <<EOF
-    import model SM_LS0_LS1_UFO
-    define l+ = e+ mu+ ta+
-    define l- = e- mu- ta-
-    generate p p > w+ w+ p p QED=4 QCD=99, w+ > l+ vl
-    add process p p > w- w- p p QED=4 QCD=99, w- > l- vl~
-    output qed_4_qcd_99_ls0ls1_grid
-    launch qed_4_qcd_99_ls0ls1_grid
-    reweight=ON
-    /scratch/anlevin/UserCode/madgraph_generation/run_card.dat
-    /scratch/anlevin/UserCode/madgraph_generation/param_card.dat
-    /scratch/anlevin/UserCode/madgraph_generation/reweight_card.dat
-    set clusinfo F
-    set nevents 100
-    set iseed $seed
-    EOF
+set nb_core 2
+import $model
+define l+ = e+ mu+ ta+
+define l- = e- mu- ta-
+$gen_command1
+$gen_command2
+output mg_dir
+launch mg_dir
+reweight=ON
+/scratch/anlevin/UserCode/madgraph_generation/run_card.dat
+/scratch/anlevin/UserCode/madgraph_generation/param_card.dat
+$reweight_file
+set clusinfo F
+set nevents $n_events_per_job
+set iseed $seed
+EOF
 else
     echo "running on unknown machine, exiting"
     exit
@@ -82,16 +108,10 @@ cat commands.mg5
     
 python2.6 ./bin/mg5_aMC commands.mg5
 
-gunzip qed_4_qcd_99_ls0ls1_grid/Events/run_01/unweighted_events.lhe.gz
+gunzip mg_dir/Events/run_01/unweighted_events.lhe.gz
 
-if hostname | grep 'lxplus.*\.cern\.ch' >& /dev/null; then
-    echo "running on lxplus"
-    cp qed_4_qcd_99_ls0ls1_grid/Events/run_01/unweighted_events.lhe /afs/cern.ch/work/a/anlevin/tmp/unweighted_events_${seed}.lhe
-elif hostname | grep 'mit\.edu' &> /dev/null; then
-    echo "running at MIT"
-    cp qed_4_qcd_99_ls0ls1_grid/Events/run_01/unweighted_events.lhe /scratch/anlevin/tmp/unweighted_events_${seed}.lhe
-fi
-    
+cp mg_dir/Events/run_01/unweighted_events.lhe ${output_dir}unweighted_events_${seed}.lhe
+
 cd ../
 
 echo "pwd"
@@ -108,7 +128,7 @@ echo "removing the madgraph directory"
 
 echo ""
 
-rm -r 2.0.0beta4/
+rm -r MG5_aMC_v2_0_0/
 
 echo "ls"
 ls
